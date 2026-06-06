50ae9e1d478da39b0eeee26a83f4920a82cf67c5
Sonali-Patel98 <sonalipatel9845@gmail.com>
2026-06-06 17:20:34 +0530

    web hooks

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 //App Config
 const PORT = process.env.PORT || 4000
@@ -17,4 +18,5 @@ app.use(cors())
 app.get('/',(req,res)=>
     res.send("API is Working")
 )
+app.use('/api/user',userRouter)
 app.listen(PORT,()=>console.log("Server is running on Port" + PORT))
